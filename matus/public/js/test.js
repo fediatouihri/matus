a758e60f156b90e10a71d3615ce5fc42fde6d5b4
fedia touihri <fedia.touihri@enicar.u-carthage.tn>
2021-08-02 17:56:47 +0100

    funct telecharger()

---
+++ matus/public/js/test.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+function telecharger(){
+for (var i=0;i<mydata.length;i++){
+
+  if(mydata[i].label.length==7){
+       mydatasept.push(mydata[i].label);
+  }   
+}
+
+
+var mots=[];
+var nb_mots=mydatasept.length;
+for(var i=0;i<10;i++){
+     var rand= Math.floor(Math.random() *nb_mots);
+     mots.push(mydatasept[rand]);
+     console.log(mots[i]);
+}
+
+}
+
+
+
+
+
+
+
+
---
+++ matus/resources/views/lancer.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+    <head>
+        <meta charset="utf-8">
+        <link href="{{asset('css/style.css')}}"rel="stylesheet">
+
+        
+        <title>:: Matus ::</title>
+
+       
+        <style>
+           
+        </style>
+        
+    </head>
+    <body >
+         <h1>:: Welcom Matusien ::</h1>
+         <ul style="style:none;">
+              <li>
+                   <input style="background : green;"type="button" value="commencer">
+              </li>
+         </ul>
+         <script type="text/javascript" src="{{asset('js/dict.json')}}"></script>
+         <script type="text/javascript" src="{{asset('js/dict7.json')}}"></script>
+         <script type="text/javascript" src="{{asset('js/test.js')}}"></script>
+  
+    </body>
+    </html>
